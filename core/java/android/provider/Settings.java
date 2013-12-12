5329e779cae1e4a5aa5a2353b3296e9d74a707ca
Dalingrin <dalingrin@gmail.com>
2013-12-12 03:19:48 -0600

    SystemUI: Add double-tap status bar to sleep [1/2]
    Adin's edit: format to follow AOSP Java Code Style
    
    Change-Id: I4c54718e8ec8774677d13917b3a7ffa3b4b18cb5
    Signed-off-by: Adin Kwok <adin.kwok@carbonrom.org>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4755,6 +4755,12 @@ public final class Settings {
         public static final String BACK_SWIPE_EXTENDED = "back_swipe_extended";
         
         /**
+         *  Enable statusbar double tap gesture on to put device to sleep
+         * @hide
+         */
+        public static final String DOUBLE_TAP_SLEEP_GESTURE = "double_tap_sleep_gesture";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
