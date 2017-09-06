d3a828d853f498bbdcd8765d26a0315a41c34e8e
bigrushdog <randall.rushing@gmail.com>
2017-09-06 13:09:52 +0200

    Add api to take screenshots
    abc: extracted from DUI
    add a public call to Utils
    adapted for partial screenshot
    
    Change-Id: Ie91990fb3c4ddb5f5e7e45f617c57b1ffa1526b8

--- core/java/com/android/internal/util/custom/Utils.java
+++ core/java/com/android/internal/util/custom/Utils.java
@@ -18,6 +18,7 @@ package com.android.internal.util.custom;
 
 import android.Manifest;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
@@ -29,6 +30,8 @@ import android.os.RemoteException;
 import android.os.ServiceManager;
 import android.os.SystemProperties;
 import android.os.SystemClock;
+import android.view.IWindowManager;
+import android.view.WindowManagerGlobal;
 
 import com.android.internal.R;
 import com.android.internal.statusbar.IStatusBarService;
@@ -36,6 +39,9 @@ import com.android.internal.statusbar.IStatusBarService;
 import java.util.Locale;
 
 public class Utils {
+
+    public static final String INTENT_SCREENSHOT = "action_take_screenshot";
+    public static final String INTENT_REGION_SCREENSHOT = "action_take_region_screenshot";
 
     // Check to see if device is WiFi only
     public static boolean isWifiOnly(Context context) {
@@ -152,6 +158,15 @@ public class Utils {
                     // do nothing.
                 }
             }
+        }
+    }
+
+    public static void takeScreenshot(boolean full) {
+        IWindowManager wm = WindowManagerGlobal.getWindowManagerService();
+        try {
+            wm.sendCustomAction(new Intent(full? INTENT_SCREENSHOT : INTENT_REGION_SCREENSHOT));
+        } catch (RemoteException e) {
+            e.printStackTrace();
         }
     }
 }
