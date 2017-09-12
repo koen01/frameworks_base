68a18aa27b93554b926f8dd56844a7db56f5bf83
Alex Naidis <alex.naidis@linux.com>
2017-09-12 14:04:27 +0800

    Immersive Recents [1/2]
    May expand this to other shit but for now, it do what it do hahaha
    
    0 - Default AOSP
    1 - Fullscreen
    2 - Statusbar only
    3 - NavBar only
    
    Change-Id: Ibd7bd84da2166dbab02cdfa5c17991f17ba8de2d

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5179,6 +5179,16 @@ public final class Settings {
         private static final Validator BATTERY_LIGHT_BLEND_REVERSE_VALIDATOR = BOOLEAN_VALIDATOR;
 
         /**
+         * Immersive recents options
+         *
+         * 0 = Default AOSP look
+         * 1 = Statusbar only
+         * 2 = Navbar only
+         * 3 = Fullscreen
+         */
+        public static final String IMMERSIVE_RECENTS = "immersive_recents";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
