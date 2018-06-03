49e36c687adb2d2a401f5d5628a79cae515d72f1
ezio84 <brabus84@gmail.com>
2018-06-04 00:36:00 +0200

    Ambient music ticker AoD: make it compatibile with double tap sensor
    also don't wake up on skip track events when pulsing Ambient
    
    TLDR: some devices like the Pixel2xl have a specific double tap sensor for AoD
    that handles the touches in this screen state and nothing is dispatched
    to StatusBarWindowView, and it's enabled by the double tap to wake
    option available in Settings - System - Gestures.
    
    We can use the related onDoubleTap callback that Google already added
    on purpose to manage double taps  on the AmbientContainer (Now Playing on
    Android P) when AoD is showing, to trigger the skip track event we need.

--- packages/SystemUI/src/com/android/systemui/doze/DozeTriggers.java
+++ packages/SystemUI/src/com/android/systemui/doze/DozeTriggers.java
@@ -187,7 +187,7 @@ public class DozeTriggers implements DozeMachine.Part {
                     if (screenX != -1 && screenY != -1) {
                         mDozeHost.onSlpiTap(screenX, screenY);
                     }
-                    gentleWakeUp(pulseReason);
+                    //gentleWakeUp(pulseReason);
                 } else if (isPickup) {
                     gentleWakeUp(pulseReason);
                 } else {
@@ -560,6 +560,11 @@ public class DozeTriggers implements DozeMachine.Part {
             if (active) {
                 mMachine.requestState(DozeMachine.State.FINISH);
             }
+        }
+
+        @Override
+        public void wakeUpFromDoubleTap() {
+            gentleWakeUp(DozeLog.REASON_SENSOR_DOUBLE_TAP);
         }
     };
 }
