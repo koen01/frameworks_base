c8f93683ff8278f1604498ca29d781bc4e164c9a
maxwen <max.weninger@gmail.com>
2019-01-24 15:32:09 +0100

    [1/4] base: qs status bar header image
    Change-Id: Ie295f7f35208545dc3146b1fdc7ad697c5d70e16

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4970,6 +4970,65 @@ public final class Settings {
                 new SettingsValidators.InclusiveIntegerRangeValidator(0, 1);
 
         /**
+         * Whether to use the custom status bar header or not
+         * @hide
+         */
+        public static final String STATUS_BAR_CUSTOM_HEADER = "status_bar_custom_header";
+
+        private static final Validator STATUS_BAR_CUSTOM_HEADER_VALIDATOR =
+                BOOLEAN_VALIDATOR;
+
+        /**
+         * Whether to apply a shadow on top of the header image
+         * value is the alpha value of the shadow image is 0 -> no shadow -> 255 black
+         * @hide
+         */
+        public static final String STATUS_BAR_CUSTOM_HEADER_SHADOW =
+                "status_bar_custom_header_shadow";
+
+        private static final Validator STATUS_BAR_CUSTOM_HEADER_SHADOW_VALIDATOR =
+                ANY_INTEGER_VALIDATOR;
+
+        /**
+         * header image package to use for daylight header - package name - null if default
+         * @hide
+         */
+        public static final String STATUS_BAR_DAYLIGHT_HEADER_PACK =
+                "status_bar_daylight_header_pack";
+
+        private static final Validator STATUS_BAR_DAYLIGHT_HEADER_PACK_VALIDATOR =
+                ANY_STRING_VALIDATOR;
+
+        /**
+         * Current active provider - available currently "static" "daylight"
+         * @hide
+         */
+        public static final String STATUS_BAR_CUSTOM_HEADER_PROVIDER =
+                "status_bar_custom_header_provider";
+
+        private static final Validator STATUS_BAR_CUSTOM_HEADER_PROVIDER_VALIDATOR =
+                ANY_STRING_VALIDATOR;
+
+        /**
+         * Manual override picture to use
+         * @hide
+         */
+        public static final String STATUS_BAR_CUSTOM_HEADER_IMAGE =
+                "status_bar_custom_header_image";
+
+        private static final Validator STATUS_BAR_CUSTOM_HEADER_IMAGE_VALIDATOR =
+                ANY_STRING_VALIDATOR;
+
+        /**
+         * @hide
+         */
+        public static final String STATUS_BAR_FILE_HEADER_IMAGE =
+                "status_bar_file_header_image";
+
+        private static final Validator STATUS_BAR_FILE_HEADER_IMAGE_VALIDATOR =
+                ANY_STRING_VALIDATOR;
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
@@ -5043,6 +5102,12 @@ public final class Settings {
             LOCKSCREEN_WEATHER_SHOW_CITY,
             RECENTS_COMPONENT,
             NOTIFICATION_LIGHT_PULSE,
+            STATUS_BAR_CUSTOM_HEADER,
+            STATUS_BAR_CUSTOM_HEADER_SHADOW,
+            STATUS_BAR_DAYLIGHT_HEADER_PACK,
+            STATUS_BAR_CUSTOM_HEADER_PROVIDER,
+            STATUS_BAR_CUSTOM_HEADER_IMAGE,
+            STATUS_BAR_FILE_HEADER_IMAGE
         };
 
         /**
@@ -5189,6 +5254,12 @@ public final class Settings {
             PRIVATE_SETTINGS.add(LOCKSCREEN_WEATHER_SHOW_TEMP);
             PRIVATE_SETTINGS.add(LOCKSCREEN_WEATHER_SHOW_CITY);
             PRIVATE_SETTINGS.add(RECENTS_COMPONENT);
+            PRIVATE_SETTINGS.add(STATUS_BAR_CUSTOM_HEADER);
+            PRIVATE_SETTINGS.add(STATUS_BAR_CUSTOM_HEADER_SHADOW);
+            PRIVATE_SETTINGS.add(STATUS_BAR_DAYLIGHT_HEADER_PACK);
+            PRIVATE_SETTINGS.add(STATUS_BAR_CUSTOM_HEADER_PROVIDER);
+            PRIVATE_SETTINGS.add(STATUS_BAR_CUSTOM_HEADER_IMAGE);
+            PRIVATE_SETTINGS.add(STATUS_BAR_FILE_HEADER_IMAGE);
         }
 
         /**
@@ -5301,6 +5372,18 @@ public final class Settings {
             VALIDATORS.put(LOCKSCREEN_WEATHER_SHOW_CITY, LOCKSCREEN_WEATHER_SHOW_CITY_VALIDATOR);
             VALIDATORS.put(RECENTS_COMPONENT,RECENTS_COMPONENT_VALIDATOR);
             VALIDATORS.put(NOTIFICATION_LIGHT_PULSE, BOOLEAN_VALIDATOR);
+            VALIDATORS.put(STATUS_BAR_CUSTOM_HEADER,
+                    STATUS_BAR_CUSTOM_HEADER_VALIDATOR);
+            VALIDATORS.put(STATUS_BAR_CUSTOM_HEADER_SHADOW,
+                    STATUS_BAR_CUSTOM_HEADER_SHADOW_VALIDATOR);
+            VALIDATORS.put(STATUS_BAR_DAYLIGHT_HEADER_PACK,
+                    STATUS_BAR_DAYLIGHT_HEADER_PACK_VALIDATOR);
+            VALIDATORS.put(STATUS_BAR_CUSTOM_HEADER_PROVIDER,
+                    STATUS_BAR_CUSTOM_HEADER_PROVIDER_VALIDATOR);
+            VALIDATORS.put(STATUS_BAR_CUSTOM_HEADER_IMAGE,
+                    STATUS_BAR_CUSTOM_HEADER_IMAGE_VALIDATOR);
+            VALIDATORS.put(STATUS_BAR_FILE_HEADER_IMAGE,
+                    STATUS_BAR_FILE_HEADER_IMAGE_VALIDATOR);
         }
 
         /**
