a9bbde7bb6adc5c46c47e4bd1a8d19cd5a7559cc
Alex <du.alexcruz@gmail.com>
2019-02-14 22:20:59 -0600

    ActionUtils: Add screen on action
    Add an action to force the screen to turn on. This is a quick
    and effective way to turn on the screen when we need it to. Intead
    of hijacking WindowManagerPolicy in PWM, we instead call on PM to
    do this.
    
    - Make sure 'screen off' action doesn't work while screen is off.
    This action should not be called if the screen is already off.
    
    Change-Id: Icacb721ebc6493e4bd0db9d02ac10d66f22eb143

--- core/java/com/android/internal/util/custom/Utils.java
+++ core/java/com/android/internal/util/custom/Utils.java
@@ -120,11 +120,18 @@ public class Utils {
                Locale.CHINESE.getLanguage());
     }
 
-    // Method to turn off the screen
-    public static void switchScreenOff(Context ctx) {
-        PowerManager pm = (PowerManager) ctx.getSystemService(Context.POWER_SERVICE);
-        if (pm!= null) {
-            pm.goToSleep(SystemClock.uptimeMillis());
+    // Screen on
+    public static void switchScreenOn(Context context) {
+        PowerManager pm = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
+        if (pm == null) return;
+        PowerManager.WakeLock wakeLock = pm.newWakeLock((
+                PowerManager.SCREEN_BRIGHT_WAKE_LOCK |
+                        PowerManager.ACQUIRE_CAUSES_WAKEUP), "wakeup:device");
+        boolean isScreenOn = pm.isScreenOn();
+        /* Wake up the device only when screen is off.
+         * Otherwise don't bother to do anything. */
+        if (!wakeLock.isHeld() && !isScreenOn) {
+            wakeLock.acquire();
         }
     }
 
@@ -158,6 +165,14 @@ public class Utils {
                     // do nothing.
                 }
             }
+        }
+    }
+
+    // Method to turn off the screen
+    public static void switchScreenOff(Context ctx) {
+        PowerManager pm = (PowerManager) ctx.getSystemService(Context.POWER_SERVICE);
+        if (pm!= null && pm.isScreenOn()) {
+            pm.goToSleep(SystemClock.uptimeMillis());
         }
     }
 
