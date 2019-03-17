DirtyUnicorns/android_frameworks_base

Code improvements for application fall-back
When updating an application two intents are sent
subsequently: PACKAGE_REMOVED -> PACKAGE_ADDED.

Add a simple check that prevents updating app to
trigger the fall-back logic.

Change-Id: I62546044e1359c7acaedf0fda58aaa26201973ef

packages/SystemUI/src/com/android/systemui/du/AppPickerServices.java
@@ -5,7 +5,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.provider.Settings;
-
+import com.android.internal.util.du.Utils;
 import com.android.systemui.R;
 import com.android.systemui.VendorServices;
 
@@ -29,6 +29,12 @@ public class AppPickerServices extends VendorServices {
             if (action.equals(Intent.ACTION_PACKAGE_REMOVED)) {
 
                 String packageName = intent.getData().getSchemeSpecificPart();
+
+                // If the package is still installed
+                if (Utils.isPackageInstalled(context, packageName)) {
+                    // it's an application update, we can skip the rest.
+                    return;
+                }
 
                 String backLongPress = Settings.System.getString(context.getContentResolver(),
                         Settings.System.KEY_BACK_LONG_PRESS_CUSTOM_APP);

koen01/frameworks_base

packages/SystemUI/src/com/android/systemui/scorpion/AppPickerServices.java
@@ -5,7 +5,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.provider.Settings;
-
+import com.android.internal.util.scorpion.Utils;
 import com.android.systemui.R;
 import com.android.systemui.VendorServices;
 
@@ -29,6 +29,12 @@ public class AppPickerServices extends VendorServices {
             if (action.equals(Intent.ACTION_PACKAGE_REMOVED)) {
 
                 String packageName = intent.getData().getSchemeSpecificPart();
+
+                // If the package is still installed
+                if (Utils.isPackageInstalled(context, packageName)) {
+                    // it's an application update, we can skip the rest.
+                    return;
+                }
 
                 String backLongPress = Settings.System.getString(context.getContentResolver(),
                         Settings.System.KEY_BACK_LONG_PRESS_CUSTOM_APP);