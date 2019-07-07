87e4364568198c8f08b0ac43ad0a23838651d84f
Pranav Vashi <neobuddy89@gmail.com>
2019-07-07 12:11:43 +0530

    Disable long click for SoundSearch tile
    Change-Id: Id893722185fa4f111f2730849d1c8abfffa121dc
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -42,7 +42,7 @@ import com.android.systemui.qs.tiles.LocationTile;
 import com.android.systemui.qs.tiles.NfcTile;
 import com.android.systemui.qs.tiles.NightDisplayTile;
 import com.android.systemui.qs.tiles.RotationLockTile;
-import com.android.systemui.qs.tiles.SoundSearchTIle;
+import com.android.systemui.qs.tiles.SoundSearchTile;
 import com.android.systemui.qs.tiles.UiModeNightTile;
 import com.android.systemui.qs.tiles.UserTile;
 import com.android.systemui.qs.tiles.WifiTile;
@@ -78,7 +78,7 @@ public class QSFactoryImpl implements QSFactory {
     private final Provider<GarbageMonitor.MemoryTile> mMemoryTileProvider;
     private final Provider<UiModeNightTile> mUiModeNightTileProvider;
     private final Provider<CaffeineTile> mCaffeineTileProvider;
-    private final Provider<SoundSearchTIle> mSoundSearchTIleProvider;
+    private final Provider<SoundSearchTile> mSoundSearchTileProvider;
 
     private QSTileHost mHost;
 
@@ -103,7 +103,7 @@ public class QSFactoryImpl implements QSFactory {
             Provider<GarbageMonitor.MemoryTile> memoryTileProvider,
             Provider<UiModeNightTile> uiModeNightTileProvider,
             Provider<CaffeineTile> caffeineTileProvider,
-            Provider<SoundSearchTIle> soundSearchTIleProvider) {
+            Provider<SoundSearchTile> soundSearchTIleProvider) {
         mWifiTileProvider = wifiTileProvider;
         mBluetoothTileProvider = bluetoothTileProvider;
         mCellularTileProvider = cellularTileProvider;
@@ -124,7 +124,7 @@ public class QSFactoryImpl implements QSFactory {
         mMemoryTileProvider = memoryTileProvider;
         mUiModeNightTileProvider = uiModeNightTileProvider;
         mCaffeineTileProvider = caffeineTileProvider;
-        mSoundSearchTIleProvider = soundSearchTIleProvider;
+        mSoundSearchTileProvider = soundSearchTIleProvider;
     }
 
     public void setHost(QSTileHost host) {
@@ -181,7 +181,7 @@ public class QSFactoryImpl implements QSFactory {
             case "caffeine":
                 return mCaffeineTileProvider.get();
             case "soundsearch":
-                return mSoundSearchTIleProvider.get();
+                return mSoundSearchTileProvider.get();
         }
 
         // Intent tiles.
