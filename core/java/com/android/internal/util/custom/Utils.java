aff94bba8a96b27a0d8af74cb2300794b25cc0ff
ezio84 <brabus84@gmail.com>
2020-02-29 00:10:19 +0100

    Custom actions utils: add expand notifications action

--- core/java/com/android/internal/util/custom/Utils.java
+++ core/java/com/android/internal/util/custom/Utils.java
@@ -198,6 +198,20 @@ public class Utils {
                 } catch (RemoteException e) {}
             }
         }
+
+        // Toggle notifications panel
+        public static void toggleNotifications() {
+            IStatusBarService service = getStatusBarService();
+            if (service != null) {
+                try {
+                    service.togglePanel();
+                } catch (RemoteException e) {}
+            }
+        }
+    }
+
+    public static void toggleNotifications() {
+        FireActions.toggleNotifications();
     }
 
     public static void clearAllNotifications() {
