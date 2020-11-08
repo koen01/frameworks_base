9254c1b7d8f11b16263af0a0ad6de470c6cd993e
ezio84 <brabus84@gmail.com>
2020-11-08 10:17:01 +0100

    Ambient music: pulse on new music tracks and Now Playing info [1/2]
    * Adapt to Android 12
    
    Signed-off-by: Jayant-Deshmukh <jayantdeshmuk008@gmail.com>
    Change-Id: I064b92afe59d1a611ef42593fdd8ef7f82fa847e

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5184,6 +5184,11 @@ public final class Settings {
          */
         public static final String TORCH_POWER_BUTTON_GESTURE = "torch_power_button_gesture";
 
+        /** Whether to pulse ambient on new music tracks
+         *
+         * @hide
+         */
+        public static final String PULSE_ON_NEW_TRACKS = "pulse_on_new_tracks";
 
         /**
          * Battery style
