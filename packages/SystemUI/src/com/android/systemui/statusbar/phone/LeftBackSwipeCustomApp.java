8aa05038e1fa89bc3e78ab8b685e5097f7e74127
ezio84 <brabus84@gmail.com>
2020-11-25 19:59:56 +0100

    Advanced nav gestures: improve code and fix glitches
    fix  arrows animation glitch after triggering an action
    with the extended swipe
    
    hide the arrows before triggering a screenshot action
    
    code cleanup
    
    Change-Id: I5f01938b6b688acd14225f58ce5a2032f083f92f

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/LeftBackSwipeCustomApp.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/LeftBackSwipeCustomApp.java
@@ -6,8 +6,8 @@ import android.provider.Settings;
 import android.view.View;
 import android.widget.ListView;
 
-import com.abc.support.R;
-import com.abc.support.preferences.AppPicker;
+import com.srx.support.R;
+import com.srx.support.preferences.AppPicker;
 
 public class LeftBackSwipeCustomApp extends AppPicker {
 
