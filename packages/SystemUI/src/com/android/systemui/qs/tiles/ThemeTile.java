1fea7faa302cab5f4bea09a69c22ff04bb3ecac0
Dabug123 <clarkbrunswick55@gmail.com>
2021-01-12 00:47:51 +0100

    themes:Add Scorpion clear
    Change-Id: I91c0bafde4cc60786c1fc99f1fdf902c2da94a90

--- packages/SystemUI/src/com/android/systemui/qs/tiles/ThemeTile.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/ThemeTile.java
@@ -123,6 +123,8 @@ public class ThemeTile extends QSTileImpl<BooleanState> {
                 R.string.system_theme_style_pitchblack, "pitch_black"));
         sStyleItems.add(new ThemeTileItem(UiModeManager.MODE_NIGHT_YES, -1,
                 R.string.system_theme_style_solarizeddark, "solarized_dark"));
+        sStyleItems.add(new ThemeTileItem(UiModeManager.MODE_NIGHT_YES, -1,
+                R.string.system_theme_style_scorpionclear, "scorpion_clear"));
     }
 
     private enum Mode {
@@ -314,6 +316,15 @@ public class ThemeTile extends QSTileImpl<BooleanState> {
                     try {
                         mOverlayManager.setEnabled(solarized_dark,
                                 themeItem.uri.equals("solarized_dark"), USER_SYSTEM);
+                    } catch (RemoteException e) {
+                        e.printStackTrace();
+                    }
+                }
+                for (int i = 0; i < ThemesUtils.SCORPION_CLEAR.length; i++) {
+                    String scorpion_clear = ThemesUtils.SCORPION_CLEAR[i];
+                    try {
+                        mOverlayManager.setEnabled(scorpion_clear,
+                                themeItem.uri.equals("scorpion_clear"), USER_SYSTEM);
                     } catch (RemoteException e) {
                         e.printStackTrace();
                     }
