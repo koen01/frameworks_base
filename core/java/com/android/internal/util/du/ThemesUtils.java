3ee010e2a9f3c31704e4eb32eb980aaa24cf4608
Dabug123 <clarkbrunswick55@gmail.com>
2021-01-20 16:21:36 +0100

    fonts:add sfpro [1/3]
    Change-Id: Ifab1ffdc99d59a0c3668040612e4dd00d154579d

--- core/java/com/android/internal/util/du/ThemesUtils.java
+++ core/java/com/android/internal/util/du/ThemesUtils.java
@@ -107,5 +107,6 @@ public class ThemesUtils {
             "com.android.theme.font.exotwo",
             "com.android.theme.font.rubikrubik",
             "com.android.theme.font.comicsanssource",
+            "com.android.theme.font.sfpro",
     };
 }
